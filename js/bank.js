ddcf036047dc0a7fd41f0eb98fd6c30c18c2ab8b
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-08-17 22:02:25 +0600

    All functionality like deposit widthdrawal and balance feature added and worked well!!!

--- js/bank.js
+++ js/bank.js
@@ -5,11 +5,54 @@ deposit.addEventListener("click", function () {
     //get the ammount want to Deposit
     const deposit = document.getElementById("addMoney");
     const depositvalue = deposit.value;
-    console.log(depositvalue);
+    const userInputDeposit = parseFloat(depositvalue);
+    // const dipsositedAmmount = parseFloat(depositvalue);
+    // console.log(depositvalue);
+    //geting user inputed value and sum it with prevoius ammount
     const dispalyDeposit = document.getElementById("depositField");
+    const prevousDeposited = dispalyDeposit.innerText;
+    const previousammount = parseFloat(prevousDeposited);
+    const finalDepostit = userInputDeposit + previousammount;
 
-    dispalyDeposit.innerText = depositvalue;
+
+    dispalyDeposit.innerText = finalDepostit;
     deposit.value = "";
+
+    //balance part
+    const prevoiusbalance = document.getElementById("balaceDashboard");
+    const prevoiusbalanceFloat = parseFloat(prevoiusbalance.innerText);
+    const totalBalance = prevoiusbalanceFloat + userInputDeposit;
+    prevoiusbalance.innerText = totalBalance;
+})
+
+
+
+
+// widthdrawal part
+
+//adding widrawalButton Listener
+const Widthdrwalbtn = document.getElementById("moneyOutBtn");
+Widthdrwalbtn.addEventListener("click", function () {
+
+    //getiing Previous ammount
+    const gettingValueWidthdrawal = document.getElementById("widthdrawField");
+    const prevoiusWidthdrwal = parseFloat(gettingValueWidthdrawal.innerText);
+    //user inputed widthdrawal Ammount
+    const inputFromWidthdrawal = document.getElementById("moneyOut");
+    const userInputedWidthdrawal = parseFloat(inputFromWidthdrawal.value);
+    inputFromWidthdrawal.value = "";
+    gettingValueWidthdrawal.innerText = prevoiusWidthdrwal + userInputedWidthdrawal;
+    //balance update
+    const Gotedbalance = document.getElementById("balaceDashboard");
+    const dshboardBalace = parseFloat(Gotedbalance.innerText);
+    const finalBalance = dshboardBalace - userInputedWidthdrawal;
+    Gotedbalance.innerText = finalBalance;
+
+
 
 
 })
+
+
+
+
