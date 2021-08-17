e69201beaca7bd922f470e80a6bd407d56222afb
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-08-17 12:58:39 +0600

    deposit system Updated

--- js/bank.js
+++ js/bank.js
@@ -1,23 +1,15 @@
-const loginButton = document.getElementById("loginBtn");
-loginButton.addEventListener("click", function () {
-    const emailField = document.getElementById("user-email");
-    const userEmail = emailField.value;
-    // console.log(userEmail);
-    const passwordfield = document.getElementById('user-password');
-    const userPassword = passwordfield.value;
-    // console.log(userPassword);
-    if (userEmail == "badhon0777@gmail.com" && userPassword == "admin123") {
-        const eMessage = document.getElementById("error");
-        eMessage.innerText = "Success!!!"
-        eMessage.style.color = 'green';
-        window.location.href = 'banking.html';
+//deposit button and deposit money
+const deposit = document.getElementById("addMoneyBtn");
+console.log(deposit);
+deposit.addEventListener("click", function () {
+    //get the ammount want to Deposit
+    const deposit = document.getElementById("addMoney");
+    const depositvalue = deposit.value;
+    console.log(depositvalue);
+    const dispalyDeposit = document.getElementById("depositField");
 
-    }
-    else {
-        const eMessage = document.getElementById("error");
-        eMessage.innerText = "Something went Wrong Please checK email and password"
-        eMessage.style.color = 'red';
-    }
+    dispalyDeposit.innerText = depositvalue;
+    deposit.value = "";
 
 
 })
